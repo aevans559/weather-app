b62397f5ccde214c2bfae73dc32b401ec9cebf2d
Aaron Evans <aaronmevans14@gmail.com>
2020-05-12 18:25:57 -0500

    updated env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+require('dotenv').config({ path: 'variables.env' }); 
+
 const path = require("path");
 const axios = require("axios");
 const port = 8080;
 
-const config = require('../config.js')
+// const config = require('../config.js')
 
 app.use(express.static(path.join(process.cwd(), "/client/public")));
 
@@ -12,7 +14,7 @@ const state = 'california';
 const city = 'hanford';
 
 app.get('/location/:city.:state', (req, res) => {
-  axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.state}&units=imperial&appid=${config.apiKey}`)
+  axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.state}&units=imperial&appid=${process.env.API_KEY}`)
     .then(res => res.data)
     .then(data => res.send(data))
     .catch(err => console.log(err))
